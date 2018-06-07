cbc1ccc141dcd3e6d71a89ea7f232053e77e2092
Anthony <31975404+PhucAnthoy@users.noreply.github.com>
2018-06-07 16:28:38 -0400

    Quality Checker and GUI Updates
    Added code to check for the quality of the recipe used. Made the GUI look a bit better and improve a couple images

--- docs/js/main.coffee
+++ docs/js/main.coffee
@@ -1,6 +1,7 @@
 ingredientsInPot = 0
 ingredients = [0, 0, 0, 0, 0]
 
+# Tracks when ingredients are selected to be added to the pot
 $(document).ready ->
   $(".button.ingredients").mousedown (e) ->
     # These two lines are totally unnecessary
@@ -29,8 +30,10 @@ $(document).ready ->
             filled = $("#pot").find("div")[index].dataset.filled
             break unless test
 
+# Tracks when ingredients are selected to be removed from the pot
 $(document).ready ->
   $(".ingredient").mousedown (e) ->
+    e.preventDefault()
     filled = this.dataset.filled
     if(filled == "true")
       $(this).find("img")[0].src = ""
@@ -38,13 +41,14 @@ $(document).ready ->
       ingredientsInPot--
     if($("#output").find("img")[0].dataset.filled == "true")
       $("#output").find("img")[0].dataset.filled = "false"
-      $("#output").find("img")[0].src = ""
+      $("#output").find("div")[0].className = "button output hidden"
       $("#output").find("img")[1].dataset.filled = "false"
-      $("#output").find("img")[1].src = ""
+      $("#output").find("div")[1].className = "button output hidden"
       $("#output").find("img")[2].dataset.filled = "false"
-      $("#output").find("img")[2].src = ""
+      $("#output").find("div")[2].className = "button output hidden"
+      $("#output")[0].dataset.cooked = "false"
 
-
+# Tracks when the start cooking button is pressed and determines what dishes are possible
 $(document).ready ->
   tiny     = [1, 0, 0, 0, 1, 0, 1, 0, 0, 0]
   bluk     = [0, 1, 0, 0, 1, 0, 0, 1, 0, 0]
@@ -77,47 +81,66 @@ $(document).ready ->
   types       = ["red", "blue", "yellow", "gray", "water", "normal", "poison", "ground", "grass", "bug", "psychic", "rock", "flying", "fire", "electric", "fighting"]
 
   $("#start").mousedown (e) ->
+    e.preventDefault()
+    if($("#output")[0].dataset.cooked == "true")
+      return
     console.log(ingredients)
     if(ingredientsInPot == 5)
-      console.log("cooking")
-      total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      $("#output")[0].dataset.cooked = "true"
+      total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      quality = 0
 
+      # Checks for quality
+      # Totals the attributes of all ingredients in pot
       for ingredient in ingredients
+        if(ingredient > 4)
+          quality++
         for attribute, index in attributes[0]
           total[index] += parseInt(attributes[ingredient][index])
-          #console.log(total)
 
       count = 0
+      # Checks to see if the ingredients in the pot match any recipes
       for recipe, index in recipes
         type = index
         check = true
         for attribute, index in attributes[0]
           check &= total[index] >= recipe[index]
 
+        # Checks for open output slots
+        # Hopefully, there are no recipes with 4 possible dishes
         if(check)
           if($("#output").find("img")[0].dataset.filled == "false")
             $("#output").find("img")[0].dataset.filled = "true"
-            console.log($("#output").find("img")[0].dataset.filled)
             $("#output").find("img")[0].src = "img/dishes/" + types[type] + ".png"
-            console.log($("#output").find("img")[0].src)
+            $("#output").find("div")[0].className = "button output"
+            console.log($("#output").find("div")[0].className)
             console.log(types[type])
           else if($("#output").find("img")[1].dataset.filled == "false")
             $("#output").find("img")[1].dataset.filled = "true"
-            console.log($("#output").find("img")[1].dataset.filled)
             $("#output").find("img")[1].src = "img/dishes/" + types[type] + ".png"
-            console.log($("#output").find("img")[1].src)
+            $("#output").find("div")[1].className = "button output"
             console.log(types[type])
           else
             $("#output").find("img")[2].dataset.filled = "true"
-            console.log($("#output").find("img")[2].dataset.filled)
             $("#output").find("img")[2].src = "img/dishes/" + types[type] + ".png"
-            console.log($("#output").find("img")[2].src)
+            $("#output").find("div")[2].className = "button output"
             console.log(types[type])
         else
           count++
 
+        # Will this dish end up being mulligan?
         if(count == 16)
-          $("#output")[0].dataset.filled = "true"
-          console.log($("#output")[0].dataset.filled)
+          $("#output").find("img")[0].dataset.filled = "true"
           $("#output").find("img")[0].src = "img/dishes/mulligan.png"
+          $("#output").find("div")[0]s.className = "button output"
           console.log("mulligan")
+
+      # Quality checker
+      if(quality > 4)
+        console.log("special")
+      else if(quality > 2)
+        console.log("very good")
+      else if(quality > 0)
+        console.log("good")
+      else
+        console.log("bleh")
--- docs/js/main.js
+++ docs/js/main.js
@@ -6,6 +6,7 @@
 
   ingredients = [0, 0, 0, 0, 0];
 
+  // Tracks when ingredients are selected to be added to the pot
   $(document).ready(function() {
     return $(".button.ingredients").mousedown(function(e) {
       var filled, image, index, results, test;
@@ -46,9 +47,11 @@
     });
   });
 
+  // Tracks when ingredients are selected to be removed from the pot
   $(document).ready(function() {
     return $(".ingredient").mousedown(function(e) {
       var filled;
+      e.preventDefault();
       filled = this.dataset.filled;
       if (filled === "true") {
         $(this).find("img")[0].src = "";
@@ -57,15 +60,17 @@
       }
       if ($("#output").find("img")[0].dataset.filled === "true") {
         $("#output").find("img")[0].dataset.filled = "false";
-        $("#output").find("img")[0].src = "";
+        $("#output").find("div")[0].className = "button output hidden";
         $("#output").find("img")[1].dataset.filled = "false";
-        $("#output").find("img")[1].src = "";
+        $("#output").find("div")[1].className = "button output hidden";
         $("#output").find("img")[2].dataset.filled = "false";
-        return $("#output").find("img")[2].src = "";
+        $("#output").find("div")[2].className = "button output hidden";
+        return $("#output")[0].dataset.cooked = "false";
       }
     });
   });
 
+  // Tracks when the start cooking button is pressed and determines what dishes are possible
   $(document).ready(function() {
     var apricorn, attributes, balm, blue, bluk, bug, electric, fighting, fire, flying, fossil, grass, gray, ground, honey, icy, normal, poison, psychic, recipes, red, rock, root, tiny, types, water, yellow;
     tiny = [1, 0, 0, 0, 1, 0, 1, 0, 0, 0];
@@ -96,22 +101,31 @@
     recipes = [red, blue, yellow, gray, water, normal, poison, ground, grass, bug, psychic, rock, flying, fire, electric, fighting];
     types = ["red", "blue", "yellow", "gray", "water", "normal", "poison", "ground", "grass", "bug", "psychic", "rock", "flying", "fire", "electric", "fighting"];
     return $("#start").mousedown(function(e) {
-      var attribute, check, count, i, index, ingredient, j, k, l, len, len1, len2, len3, recipe, ref, ref1, results, total, type;
+      var attribute, check, count, i, index, ingredient, j, k, l, len, len1, len2, len3, quality, recipe, ref, ref1, total, type;
+      e.preventDefault();
+      if ($("#output")[0].dataset.cooked === "true") {
+        return;
+      }
       console.log(ingredients);
       if (ingredientsInPot === 5) {
-        console.log("cooking");
+        $("#output")[0].dataset.cooked = "true";
         total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        quality = 0;
+// Checks for quality
+// Totals the attributes of all ingredients in pot
         for (i = 0, len = ingredients.length; i < len; i++) {
           ingredient = ingredients[i];
+          if (ingredient > 4) {
+            quality++;
+          }
           ref = attributes[0];
           for (index = j = 0, len1 = ref.length; j < len1; index = ++j) {
             attribute = ref[index];
             total[index] += parseInt(attributes[ingredient][index]);
           }
         }
-        //console.log(total)
         count = 0;
-        results = [];
+// Checks to see if the ingredients in the pot match any recipes
         for (index = k = 0, len2 = recipes.length; k < len2; index = ++k) {
           recipe = recipes[index];
           type = index;
@@ -121,39 +135,47 @@
             attribute = ref1[index];
             check &= total[index] >= recipe[index];
           }
+          // Checks for open output slots
+          // Hopefully, there are no recipes with 4 possible dishes
           if (check) {
             if ($("#output").find("img")[0].dataset.filled === "false") {
               $("#output").find("img")[0].dataset.filled = "true";
-              console.log($("#output").find("img")[0].dataset.filled);
               $("#output").find("img")[0].src = "img/dishes/" + types[type] + ".png";
-              console.log($("#output").find("img")[0].src);
+              $("#output").find("div")[0].className = "button output";
+              console.log($("#output").find("div")[0].className);
               console.log(types[type]);
             } else if ($("#output").find("img")[1].dataset.filled === "false") {
               $("#output").find("img")[1].dataset.filled = "true";
-              console.log($("#output").find("img")[1].dataset.filled);
               $("#output").find("img")[1].src = "img/dishes/" + types[type] + ".png";
-              console.log($("#output").find("img")[1].src);
+              $("#output").find("div")[1].className = "button output";
               console.log(types[type]);
             } else {
               $("#output").find("img")[2].dataset.filled = "true";
-              console.log($("#output").find("img")[2].dataset.filled);
               $("#output").find("img")[2].src = "img/dishes/" + types[type] + ".png";
-              console.log($("#output").find("img")[2].src);
+              $("#output").find("div")[2].className = "button output";
               console.log(types[type]);
             }
           } else {
             count++;
           }
+          // Will this dish end up being mulligan?
           if (count === 16) {
-            $("#output")[0].dataset.filled = "true";
-            console.log($("#output")[0].dataset.filled);
+            $("#output").find("img")[0].dataset.filled = "true";
             $("#output").find("img")[0].src = "img/dishes/mulligan.png";
-            results.push(console.log("mulligan"));
-          } else {
-            results.push(void 0);
+            $("#output").find("div")[0].className = "button output";
+            console.log("mulligan");
           }
         }
-        return results;
+        // Quality checker
+        if (quality > 4) {
+          return console.log("special");
+        } else if (quality > 2) {
+          return console.log("very good");
+        } else if (quality > 0) {
+          return console.log("good");
+        } else {
+          return console.log("bleh");
+        }
       }
     });
   });
